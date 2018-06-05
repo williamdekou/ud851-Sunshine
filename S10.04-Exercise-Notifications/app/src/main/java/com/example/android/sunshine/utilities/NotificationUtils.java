3e4f3fba3547b5aea318f659794195c0da730e84
William DEKOU <williamdekou@gmail.com>
2018-06-05 07:37:36 +0100

    Add notifications to sunshine

--- S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
+++ S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
@@ -1,14 +1,23 @@
 package com.example.android.sunshine.utilities;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 
+import com.example.android.sunshine.DetailActivity;
 import com.example.android.sunshine.R;
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 
 public class NotificationUtils {
@@ -32,8 +41,8 @@ public class NotificationUtils {
     public static final int INDEX_MAX_TEMP = 1;
     public static final int INDEX_MIN_TEMP = 2;
 
-//  TODO (1) Create a constant int value to identify the notification
-
+//  COMPLETED (1) Create a constant int value to identify the notification
+    public static final int WEATHER_NOTIFICATION_ID = 1452;
     /**
      * Constructs and displays a notification for the newly updated weather for today.
      *
@@ -83,19 +92,30 @@ public class NotificationUtils {
             int smallArtResourceId = SunshineWeatherUtils
                     .getSmallArtResourceIdForWeatherCondition(weatherId);
 
-//          TODO (2) Use NotificationCompat.Builder to begin building the notification
+//          COMPLETED (2) Use NotificationCompat.Builder to begin building the notification
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setColor(ContextCompat.getColor(context,R.color.colorPrimary))
+                    .setSmallIcon(smallArtResourceId)
+                    .setLargeIcon(largeIcon)
+                    .setContentTitle(notificationTitle)
+                    .setContentText(notificationText)
+                    .setAutoCancel(true);
+//          COMPLETED (3) Create an Intent with the proper URI to start the DetailActivity
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.setData(todaysWeatherUri);
+//          COMPLETED (4) Use TaskStackBuilder to create the proper PendingIntent
+            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+            taskStackBuilder.addNextIntentWithParentStack(intent);
+            PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+//          COMPLETED (5) Set the content Intent of the NotificationBuilder
+            builder.setContentIntent(pendingIntent);
+//          COMPLETED (6) Get a reference to the NotificationManager
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-//          TODO (3) Create an Intent with the proper URI to start the DetailActivity
-
-//          TODO (4) Use TaskStackBuilder to create the proper PendingIntent
-
-//          TODO (5) Set the content Intent of the NotificationBuilder
-
-//          TODO (6) Get a reference to the NotificationManager
-
-//          TODO (7) Notify the user with the ID WEATHER_NOTIFICATION_ID
-
-//          TODO (8) Save the time at which the notification occurred using SunshinePreferences
+//          COMPLETED (7) Notify the user with the ID WEATHER_NOTIFICATION_ID
+            manager.notify(WEATHER_NOTIFICATION_ID, builder.build());
+//          COMPLETED (8) Save the time at which the notification occurred using SunshinePreferences
+            SunshinePreferences.saveLastNotificationTime(context, System.currentTimeMillis());
         }
 
         /* Always close your cursor when you're done with it to avoid wasting resources. */
